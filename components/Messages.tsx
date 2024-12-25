1f18f1cac8563d7c15814703ee3c5099c9b31f2d
Hillay Koon <iace@foxmail.com>
2024-12-26 01:21:09 +0800

    Update Messages.tsx

--- components/Messages.tsx
+++ components/Messages.tsx
@@ -10,6 +10,10 @@ const Messages = forwardRef<
   Record<never, never>
 >(function Messages(_, ref) {
   const { messages } = useVoice();
+  
+const isRole = (role: any): role is "Assistant" | "User" => {
+  return role === "Assistant" || role === "User";
+};
 
   return (
     <motion.div
@@ -53,8 +57,11 @@ const Messages = forwardRef<
                       "text-xs capitalize font-medium leading-none opacity-50 pt-4 px-3"
                     )}
                   >
-                     {msg.message.role === "Assistant" ? "Nicky" : 
-                   msg.message.role === "User" ? "You" : msg.message.role}
+                     {isRole(msg.message.role)
+                    ? msg.message.role === "Assistant"
+                      ? "Nicky"
+                      : "You"
+                    : msg.message.role}
                 </div>
                 <div className={"pb-3 px-3"}>{msg.message.content}</div>
                 {/* <Expressions values={{ ...msg.models.prosody?.scores }} /> */}
