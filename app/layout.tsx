bcf72eba543ad9d8b1e093d7dd6ba95bf5b842d9
Hillay Koon <iace@foxmail.com>
2024-12-26 01:06:00 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,8 @@ import { Nav } from "@/components/Nav";
 import { cn } from "@/utils";
 
 export const metadata: Metadata = {
-  title: "Hume AI - EVI - Next.js Starter",
-  description: "A Next.js starter using Hume AI's Empathic Voice Interface",
+  title: "Linkin.Love - Call your love",
+  description: "Call your AI lover in real-time",
 };
 
 export default function RootLayout({
