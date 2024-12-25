5bc74d936f583517ef435d5be3c49034370d813e
Hillay Koon <iace@foxmail.com>
2024-12-26 00:15:04 +0800

    Update Nav.tsx
    Change Nav

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,10 +1,9 @@
 "use client";
 
 import { useLayoutEffect, useState } from "react";
-import HumeLogo from "./logos/Hume";
+import LiLLogo from "./logos/LiL";
 import { Button } from "./ui/button";
 import { Moon, Sun } from "lucide-react";
-import Github from "./logos/GitHub";
 import pkg from '@/package.json';
 
 export const Nav = () => {
@@ -33,25 +32,9 @@ export const Nav = () => {
       }
     >
       <div>
-        <HumeLogo className={"h-5 w-auto"} />
+        <LiLLogo className={"h-5 w-auto"} />
       </div>
       <div className={"ml-auto flex items-center gap-1"}>
-        <Button
-          onClick={() => {
-            window.open(
-              pkg.homepage,
-              "_blank",
-              "noopener noreferrer"
-            );
-          }}
-          variant={"ghost"}
-          className={"ml-auto flex items-center gap-1.5"}
-        >
-          <span>
-            <Github className={"size-4"} />
-          </span>
-          <span>Star on GitHub</span>
-        </Button>
         <Button
           onClick={toggleDark}
           variant={"ghost"}
