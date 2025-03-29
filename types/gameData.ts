efd7b5cfe35e4c4d737e8a7d981b25d69ce1275b
Hillay Koon <iace@foxmail.com>
2025-03-29 11:56:15 +0800

    Update gameData.ts
    修改gameData结构体

--- types/gameData.ts
+++ types/gameData.ts
@@ -3,15 +3,11 @@ import { z } from "zod";
 // 定义 Zod Schema
 export const GameDataSchema = z.object({
   gameData: z.object({
-    choices: z.array(z.string()).optional(),
-    isTouch: z
-      .object({
-        state: z.boolean(),
-        intensity: z.number(),
-        duration: z.number(),
-      })
-      .optional(),
-    inSex: z.boolean().optional(),
+    isTouch: z.boolean(),
+    mode: z.number(),
+    preset: z.number(),
+    power: z.number(),
+    duration: z.number(),
   }),
 });
 
